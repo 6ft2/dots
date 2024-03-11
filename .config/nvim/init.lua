b93b1dd5caffde2c7d54c263aaa0b29495fada2a
skicza <162340626+skicza@users.noreply.github.com>
2024-03-11 18:05:22 -0300

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,8 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("let g:loaded_matchparen=1")
+vim.cmd("set relativenumber")
+vim.cmd("set number")
 vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
