f69cdf4556a20ede31b3d5acad9a565fab1e8b37
skicza <162340626+skicza@users.noreply.github.com>
2024-03-12 16:30:48 -0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,6 @@
-
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias ls='exa --icons --group-directories-first'
+alias v='nvim'
+starship init fish | source
