c7ed519516e67ff7e479ea6818336682fdc02483
skicza <162340626+skicza@users.noreply.github.com>
2024-03-15 16:35:26 +0000

    Add files via upload

---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("let g:loaded_matchparen=1")
+vim.cmd("set relativenumber")
+vim.cmd("set number")
+vim.g.mapleader = " "
