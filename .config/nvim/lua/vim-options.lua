12239ce6825fe9d70ceb35a4a7001dc2a911aebc
skicza <162340626+skicza@users.noreply.github.com>
2024-03-16 12:29:46 -0300

    Update vim-options.lua

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -6,3 +6,9 @@ vim.cmd("let g:loaded_matchparen=1")
 vim.cmd("set relativenumber")
 vim.cmd("set number")
 vim.g.mapleader = " "
+
+vim.cmd("nmap <Tab> :Lexplore<CR>")
+vim.cmd("let g:netrw_keepdir = 0")
+vim.cmd("let g:netrw_banner = 0")
+vim.cmd("let g:netrw_winsize = 20")
+vim.cmd("let g:netrw_localcopydircmd = 'cp -r'")
