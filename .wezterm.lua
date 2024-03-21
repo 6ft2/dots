7aaa2dddff5cb318b2b8cba68c0f9ded7b4b5038
skicza <162340626+skicza@users.noreply.github.com>
2024-03-21 18:26:34 -0300

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,8 +8,8 @@ config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.window_background_opacity = 0.9
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-	left = '1cell',
-	right = '1cell',
+	left = 0,
+	right = 0,
 	top = 0,
 	bottom = 0,
 }
